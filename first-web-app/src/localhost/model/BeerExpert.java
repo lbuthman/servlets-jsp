f9dc645fd27e9a2ed892866c95cc5351ff32a0dc
lbuthman <lbuthman@gmail.com>
2017-06-15 19:19:01 -0500

    add basic model POJO

---
+++ first-web-app/src/localhost/model/BeerExpert.java
@@ -0,0 +1,25 @@
+package localhost.model;
+
+import java.util.*;
+
+/**
+ * Created by lbuthman on 6/15/17.
+ * model class
+ */
+
+public class BeerExpert {
+
+    public List getBrands(String color) {
+        List brands = new ArrayList();
+
+        if (color.equals("amber")) {
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        } else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+
+        return brands;
+    }
+}
