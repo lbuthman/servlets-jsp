0a966816a5cb033ba09ad24fb62195a168ea2a25
lbuthman <lbuthman@gmail.com>
2017-06-15 19:19:42 -0500

    update control with model dependency

--- first-web-app/src/localhost/web/BeerSelect.java
+++ first-web-app/src/localhost/web/BeerSelect.java
@@ -3,19 +3,30 @@ package localhost.web;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.util.*;
+import localhost.model.*;
 
 /**
  * Created by lbuthman on 6/15/17.
+ * control class
  */
 public class BeerSelect extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
 
+        String c = request.getParameter("color");
+        BeerExpert be = new BeerExpert();
+        List result = be.getBrands(c);
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println("Beer Selection Advice<br>");
-        String c = request.getParameter("color");
-        out.println("<br>Got beer color " + c);
+
+        for (Object r: result) {
+            out.print("<br>try: " + r);
+        }
+
+        //out.println("<br>Got beer color " + c);
     }
 }
